17041a9267e4f0c9e9ecd83b305d5b6063b60d3c
CAI-Coach <jacques.b@stressfrei-mit-ai.de>
2026-06-18 12:54:02 +0200

    Deploy 2026-06-18T10:54:01.519Z

--- _next/static/chunks/app/not-found-1ed21af8f9f0d3c9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,308,795,648,931],{5581:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=5581)}),_N_E=n.O()}]);
